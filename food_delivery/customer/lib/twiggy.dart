f3fd61096a1b17ea691f58649ed7e4b551827206
Mohammad Farseen <farseenmanekhan1232@gmail.com>
2023-09-28 11:55:43 +0530

    adding files

---
+++ food_delivery/customer/lib/twiggy.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Twiggy extends StatelessWidget {
+  const Twiggy({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Text("temp");
+  }
+}
